805ba93a519c060bfc17902851d29972a7a3e89a
JAcutillas <ja.cutillas@iesdoctorbalmis.com>
2022-01-17 13:56:51 +0100

    commit

--- Personas/servicios/NavegacionServicio.cs
+++ Personas/servicios/NavegacionServicio.cs
@@ -1,9 +1,4 @@
 ﻿using Personas.ventanas;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace Personas.servicios
